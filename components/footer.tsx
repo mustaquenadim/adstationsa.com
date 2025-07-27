c7f8152b557e9123341291c8d28ed0c4f3acd43a
mustaquenadim <mustaquenadim2000@gmail.com>
2025-07-27 11:51:36 +0300

    :art: Refine Footer Layout and Update Icon Sizes for Improved Responsiveness

--- components/footer.tsx
+++ components/footer.tsx
@@ -13,108 +13,113 @@ const Footer = () => {
   return (
     <footer className="bg-black text-white">
       {/* Navigation and Calls to Action */}
-      <div className="max-w-7xl mx-auto px-4 py-12">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 sm:py-12">
         {/* Navigation Links */}
-        <div className="flex justify-center space-x-8 mb-6 rtl:space-x-reverse">
+        <div className="flex flex-col sm:flex-row justify-center space-y-4 sm:space-y-0 sm:space-x-8 mb-6 rtl:sm:space-x-reverse">
           <a
             href="#"
-            className="text-white hover:text-gray-300 transition-colors"
+            className="text-white hover:text-gray-300 transition-colors text-center sm:text-left"
           >
             About Us
           </a>
           <a
             href="#"
-            className="text-white hover:text-gray-300 transition-colors"
+            className="text-white hover:text-gray-300 transition-colors text-center sm:text-left"
           >
             Services
           </a>
           <a
             href="#"
-            className="text-white hover:text-gray-300 transition-colors"
+            className="text-white hover:text-gray-300 transition-colors text-center sm:text-left"
           >
             Why Adstation
           </a>
           <a
             href="#"
-            className="text-white hover:text-gray-300 transition-colors"
+            className="text-white hover:text-gray-300 transition-colors text-center sm:text-left"
           >
             Blog
           </a>
           <a
             href="#"
-            className="text-white hover:text-gray-300 transition-colors"
+            className="text-white hover:text-gray-300 transition-colors text-center sm:text-left"
           >
             Contact Us
           </a>
         </div>
 
         {/* Calls to Action */}
-        <div className="flex justify-center space-x-6 mb-8 rtl:space-x-reverse">
+        <div className="flex flex-col sm:flex-row justify-center space-y-4 sm:space-y-0 sm:space-x-6 mb-8 rtl:sm:space-x-reverse">
           <a
             href="#"
-            className="text-white hover:text-gray-300 transition-colors"
+            className="text-white hover:text-gray-300 transition-colors text-center sm:text-left"
           >
             Become a Partner
           </a>
           <a
             href="#"
-            className="text-white hover:text-gray-300 transition-colors"
+            className="text-white hover:text-gray-300 transition-colors text-center sm:text-left"
           >
             Get a Quote
           </a>
         </div>
 
         {/* Social Media Icons */}
-        <div className="flex justify-center space-x-6 mb-12 rtl:space-x-reverse">
+        <div className="flex justify-center space-x-4 sm:space-x-6 mb-8 sm:mb-12 rtl:space-x-reverse">
           <a
             href="#"
             className="text-white hover:text-gray-300 transition-colors"
           >
-            <Facebook size="24" color="white" />
+            <Facebook size="20" className="sm:w-6 sm:h-6" color="white" />
           </a>
           <a
             href="#"
             className="text-white hover:text-gray-300 transition-colors"
           >
-            <Instagram size="24" color="white" />
+            <Instagram size="20" className="sm:w-6 sm:h-6" color="white" />
           </a>
           <a
             href="#"
             className="text-white hover:text-gray-300 transition-colors"
           >
-            <X size="24" color="white" />
+            <X size="20" className="sm:w-6 sm:h-6" color="white" />
           </a>
           <a
             href="#"
             className="text-white hover:text-gray-300 transition-colors"
           >
-            <Pinterest size="24" color="white" />
+            <Pinterest size="20" className="sm:w-6 sm:h-6" color="white" />
           </a>
           <a
             href="#"
             className="text-white hover:text-gray-300 transition-colors"
           >
-            <TikTok size="24" color="white" />
+            <TikTok size="20" className="sm:w-6 sm:h-6" color="white" />
           </a>
           <a
             href="#"
             className="text-white hover:text-gray-300 transition-colors"
           >
-            <YouTube size="24" color="white" />
+            <YouTube size="20" className="sm:w-6 sm:h-6" color="white" />
           </a>
         </div>
 
         {/* Business Logos */}
-        <div className="flex justify-center items-center gap-10 pt-8">
+        <div className="flex flex-col sm:flex-row justify-center items-center gap-6 sm:gap-10 pt-6 sm:pt-8">
           {/* Saudi Business Center Logo */}
-          <SaudiBusinessCenter size="100" color="white" />
+          <SaudiBusinessCenter
+            size="80"
+            className="sm:w-24 sm:h-24 lg:w-25 lg:h-25"
+            color="white"
+          />
 
           {/* Ameyez Logo */}
           <Image
             src="/images/ameyez.png"
             alt="Ameyez"
-            width={160}
-            height={160}
+            width={120}
+            height={120}
+            className="w-20 h-20 sm:w-32 sm:h-32 lg:w-40 lg:h-40 object-contain"
           />
         </div>
       </div>
