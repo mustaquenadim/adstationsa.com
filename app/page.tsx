ae2dfc676f70281c28d2739381d152ca68146f95
mustaquenadim <mustaquenadim2000@gmail.com>
2025-07-27 10:55:53 +0300

    :sparkles: Add 'Five' Section with Product Display Solutions and Background Image

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
       <Two />
       <Three />
       <Four />
+      <Five />
     </>
   );
 }
@@ -96,15 +97,48 @@ const Four = () => {
       {/* Content */}
       <div className="relative z-20 text-left text-white px-8 max-w-2xl">
         <h2 className="text-4xl md:text-6xl font-bold mb-4 drop-shadow-lg">
-          Explore over 70 advertising services waiting for you
+          Product and application logo design services
         </h2>
         <p className="text-lg md:text-2xl mb-6 drop-shadow">
-          Adstation is an integrated platform specializing in advertising
-          requests, bringing together the largest group of advertising agencies
-          and companies, all in one place, according to professional standards.
+          Adstation provides factories and importers with specialized solutions
+          in product design and its various applications, from logo to
+          distribution. Experienced agencies.
         </p>
         <Button className="bg-primary hover:bg-primary/80 text-white font-semibold py-3 px-8 rounded-lg shadow-lg transition">
           Explore Services
+        </Button>
+      </div>
+    </section>
+  );
+};
+
+const Five = () => {
+  return (
+    <section className="relative w-full h-screen flex items-center justify-start overflow-hidden bg-[#8dc73f]">
+      {/* Background Image */}
+      <Image
+        src="/bg-five.png"
+        alt="Hero Background"
+        layout="fill"
+        objectFit="cover"
+        quality={90}
+        className="z-0 scale-x-[-1]"
+        priority
+      />
+
+      {/* Content */}
+      <div className="relative z-20 text-left text-white px-8 max-w-2xl">
+        <h2 className="text-4xl md:text-6xl font-bold mb-4 drop-shadow-lg">
+          Point of Sale Product Display Solutions
+        </h2>
+        <p className="text-lg md:text-2xl mb-6 drop-shadow">
+          Adstation has partnerships with companies, factories, and professional
+          designers specializing in display system applications and solutions
+          that distinguish your products at points of sale in grocery stores,
+          supermarkets, and other stores.
+        </p>
+        <Button className="bg-primary hover:bg-primary/80 text-white font-semibold py-3 px-8 rounded-lg shadow-lg transition">
+          Get it Now
         </Button>
       </div>
     </section>
