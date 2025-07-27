932110ab4449b231783309a3450e4c750d3ab8a6
mustaquenadim <mustaquenadim2000@gmail.com>
2025-07-27 11:59:32 +0300

    :sparkles: Add Arabic Font Support with Montserrat Variants and Update Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
@@ -7,6 +8,58 @@ import Footer from "@/components/footer";
 const montserrat = Montserrat({
   variable: "--font-montserrat",
   subsets: ["latin"],
+});
+
+const montserratArabic = localFont({
+  src: [
+    {
+      path: "../public/fonts/ArbFONTS-Montserrat-Arabic-Thin.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/ArbFONTS-Montserrat-Arabic-ExtraLight.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/ArbFONTS-Montserrat-Arabic-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/ArbFONTS-Montserrat-Arabic-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/ArbFONTS-Montserrat-Arabic-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/ArbFONTS-Montserrat-Arabic-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/ArbFONTS-Montserrat-Arabic-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/ArbFONTS-Montserrat-Arabic-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/ArbFONTS-Montserrat-Arabic-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-montserrat-arabic",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -21,7 +74,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${montserrat.variable} antialiased font-sans`}>
+      <body
+        className={`${montserrat.variable} ${montserratArabic.variable} antialiased font-arabic`}
+      >
         <Header />
         {children}
         <Footer />
